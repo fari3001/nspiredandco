474d991c3f36a5cfcef863481b3688dc5f480bea
Farhaan <fari3001_14@hotmail.com>
2025-08-13 14:24:41 +1000

    Conditional to prevent null error

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const checkbox = document.querySelector('input[name="acknowledgment"]');
   const sendBtn = document.querySelector('button[type="submit"]');
 
+ if (!checkbox || !sendBtn) {
+    console.error('Checkbox or button not found in the DOM');
+    return;
+  }
+
   sendBtn.disabled = !checkbox.checked;
 
   checkbox.addEventListener('change', () => {
